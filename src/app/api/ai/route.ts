3c0056ce35cfb8b4b263a1c046aa1ed55b1e4930
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-13 01:16:28 -0300

    feat: add samples

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -19,3 +19,72 @@ export async function POST(request: NextRequest) {
 
   return result.toDataStreamResponse()
 }
+
+// Gerando resposta em texto: 
+// export async function GET(request: NextRequest) {
+//   const result = await generateText({
+//     model: openai('gpt-4o'),
+//     prompt: 'Traduza "Hello World" para português!',
+//     system: 'Você é uma AI especializada em tradução, sempre retorne da maneira sucinta possível.'
+//   })
+
+//   return NextResponse.json({ message: result.text })
+// }
+
+// Gerando dados estruturados: 
+// export async function GET(request: NextRequest) {
+//   const result = await generateObject({
+//     model: openrouter.chat('openai/gpt-4o-2024-11-20'),
+//     schema: z.object({
+//       pt: z.string().describe("Tradução para português"),
+//       fr: z.string().describe("Tradução para francês"),
+//       es: z.string().describe("Tradução para espanhol"),
+//     }),
+//     prompt: 'Traduza "Hello World" para diferentes idiomas!',
+//     system: 'Você é uma AI especializada em tradução, sempre retorne da maneira sucinta possível.'
+//   })
+
+//   return NextResponse.json({ message: result.object })
+// }
+
+// Generate with Tool Calling
+// export async function GET(request: NextRequest) {
+//   const result = await generateText({
+//     model: openrouter.chat('openai/gpt-4o-2024-11-20'),
+//     tools: {
+//       profileAndUrls: tool({
+//         description: 'Essa ferramenta serve para buscar do perfil de um usuário do GitHub ou acessar URLs da API para outras informações de um usuário como lista de organizações, repositórios, eventos, seguidores, seguindo, etc...',
+//         parameters: z.object({
+//           username: z.string().describe('Username do usuário no GitHub'),
+//         }),
+//         execute: async ({ username }) => {
+//           const response = await fetch(`https://api.github.com/users/${username}`)
+//           const data = await response.json()
+
+//           return JSON.stringify(data)
+//         }
+//       }),
+
+//       fetchHTTP: tool({
+//         description: "Essa ferramenta serve para realizar uma requisição HTTP em uma URL especificada e acessar sua resposta",
+//         parameters: z.object({
+//           url: z.string().describe('URL a ser requisitada'),
+//         }),
+//         execute: async ({ url }) => {
+//           const response = await fetch(url)
+//           const data = await response.text()
+
+//           return data
+//         }
+//       })
+//     },
+//     prompt: 'Me dê uma lista de usuários que o usuário diego3g segue no GitHub?',
+//     maxSteps: 5,
+
+//     onStepFinish({ toolResults }) {
+//       console.log(toolResults)
+//     }
+//   })
+
+//   return NextResponse.json({ message: result.text, parts: result.toolResults })
+// }
